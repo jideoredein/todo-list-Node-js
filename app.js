a7234e0de2042f6118afb5c888386dc64aca3deb
Jide Oredein <jideoredein@gmail.com>
2022-07-14 14:29:15 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
 let date = require(__dirname + "/date.js");
 
@@ -135,7 +136,7 @@ app.post("/delete", function(req, res){
 // *** create dynamically generated pages *** 
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
     if(!err){
